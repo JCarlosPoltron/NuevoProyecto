af3f441b0ccbf93a43527ac24b62e8c73817bb23
unknown <j-carlos_ss@hotmail.com>
2019-11-04 20:25:17 -0600

    Se agrego nueva funcion Y.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ float** sumaMatrices(float** X, int m1, int n1,float** Y, int m2, int n2);
 float** restaMatrices(float** X, int m1, int n1,float** Y, int m2, int n2);
 void nuevaFuncionDePrueba();
 void funcionX();
+void funcionY();
 
 int main(void){
     int m1,n1,m2,n2;
